210ce78735353bbec938cdf19122dbc64bd02e5c
salvomc <smcrwrk@gmail.com>
2023-07-03 18:05:51 +0200

    snack-3 done

---
+++ snack-3/index.php
@@ -0,0 +1,69 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+                        [
+                            'title' => 'Post 1',
+                            'author' => 'Michele Papagni',
+                            'text' => 'Testo post 1'
+                        ],
+                        [
+                            'title' => 'Post 2',
+                            'author' => 'Michele Papagni',
+                            'text' => 'Testo post 2'
+                        ],
+                    ],
+    '10/02/2019' => [
+                        [
+                            'title' => 'Post 3',
+                            'author' => 'Michele Papagni',
+                            'text' => 'Testo post 3'
+                        ]
+                    ],
+    '15/05/2019' => [
+                        [
+                            'title' => 'Post 4',
+                            'author' => 'Michele Papagni',
+                            'text' => 'Testo post 4'
+                        ],
+                        [
+                            'title' => 'Post 5',
+                            'author' => 'Michele Papagni',
+                            'text' => 'Testo post 5'
+                        ],
+                        [
+                            'title' => 'Post 6',
+                            'author' => 'Michele Papagni',
+                            'text' => 'Testo post 6'
+                        ]
+                    ],
+];
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>snack-3</title>
+    </head>
+    <body>
+        <div>
+            <?php foreach($posts as $date => $list) 
+            { ?>
+                <div>
+                    <?php echo $date."<br>";
+                        foreach($list as $post)         
+                        {
+                            echo $post['title']."<br>";
+                            echo $post['author']."<br>";
+                            echo $post['text']."<br>";
+                        } ?>
+                </div>
+                <br>
+            <?php  
+            } ?>
+        </div>
+    </body>
+</html>
